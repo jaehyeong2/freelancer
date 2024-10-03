a68044113c4b854f759e992ce7050fe74bc0f45e
jaehyeong <wogud1514@gmail.com>
2024-10-03 12:45:52 +0900

    feature: 프리랜서 / 클라이언트 모듈 분리

--- client-api/src/main/java/jjfactory/freelancer/presentation/FreelancerApi.java
+++ client-api/src/main/java/jjfactory/freelancer/presentation/FreelancerApi.java
@@ -1,10 +1,7 @@
 package jjfactory.freelancer.presentation;
 
 import jjfactory.freelancer.application.FreelancerFacade;
-import jjfactory.freelancer.domain.FreelancerCommand;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FreelancerApi {
     private final FreelancerFacade freelancerFacade;
-    private final FreelancerMapper freelancerMapper;
 
-    @PostMapping
-    public Long join(@RequestBody FreelancerDto.Create dto) {
-        FreelancerCommand.Create command = freelancerMapper.convert(dto);
-        return freelancerFacade.join(command);
-    }
+
 }
--- freelancer-api/src/main/java/jjfactory/freelancer/application/FreelancerFacade.java
+++ freelancer-api/src/main/java/jjfactory/freelancer/application/FreelancerFacade.java
@@ -1,5 +1,6 @@
 package jjfactory.freelancer.application;
 
+import jjfactory.freelancer.domain.FreelancerCommand;
 import jjfactory.freelancer.domain.FreelancerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -9,5 +10,7 @@ import org.springframework.stereotype.Component;
 public class FreelancerFacade {
     private final FreelancerService freelancerService;
 
-
+    public Long join(FreelancerCommand.Create command){
+        return freelancerService.store(command);
+    }
 }
